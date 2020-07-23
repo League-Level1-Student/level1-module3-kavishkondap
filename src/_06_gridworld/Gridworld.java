d219cff033f8a66eb5926e110d51abd222f1e649
kavish09 <54075256+kavish09@users.noreply.github.com>
2020-07-23 11:28:16 -0700

    Level 1 Module 3 Flappy Bird In Progress

--- src/_06_gridworld/Gridworld.java
+++ src/_06_gridworld/Gridworld.java
@@ -11,16 +11,30 @@ public class Gridworld {
 public static void main(String[] args) {
 	World world = new World ();
 	world.show();
-	Bug bug = new Bug ();
-	Location location = new Location (0, 0);
-	world.add(location, bug);
-	Bug bug2 = new Bug ();
-	Location location3 = world.getRandomEmptyLocation();
-	world.add(location3, bug2);
-	bug2.setColor(Color.BLUE);
-	bug2.setDirection(90);
+//	Bug bug = new Bug ();
+//	Location location = new Location (0, 0);
+//	world.add(location, bug);
+//	Bug bug2 = new Bug ();
+//	Location location3 = world.getRandomEmptyLocation();
+//	world.add(location3, bug2);
+//	bug2.setColor(Color.BLUE);
+//	bug2.setDirection(90);
+//	Flower flowers = new Flower ();
+//	Location location2 = new Location (location3.getRow(), location3.getCol()+1);
+//	Flower flowerstuff = new Flower ();
+//	Location location4 = new Location (location3.getRow(), location3.getCol() -1);
+//	world.add(location4, flowerstuff);
+//	world.add(location2, flowers);
+for (int i = 0; i < 100; i++) {
 	Flower flower = new Flower ();
-	Location location2 = new Location (location3.getCol()+1, location3.getRow()+1);
-	world.add(location2, flower);
+ Location location = world.getRandomEmptyLocation();
+	world.add(location, flower);
+if(location.getRow() %2==0) {
+	flower.setColor(Color.RED);
+	
+}else {
+	flower.setColor(Color.WHITE);
+}
+}
 }
 }
--- src/_07_cow_timer/CowTimer.java
+++ src/_07_cow_timer/CowTimer.java
@@ -14,7 +14,9 @@ public class CowTimer {
 	 * 1. Make a constructor for the CowTimer class that initializes the minutes
 	 * variable
 	 */
-
+CowTimer (int minutes){
+	this.minutes = minutes;
+}
 	/* 4. Complete the main method of the CowTimerRunner class */
 
 	private int minutes;
@@ -29,7 +31,14 @@ public class CowTimer {
 		 * 2. Count down the minutes, print the current minute then sleep for the number
 		 * of minutes using Thread.sleep(int milliseconds).
 		 */
-
+for (int i = minutes; i >= 0; i--) {
+	System.out.println(i);
+	Thread.sleep(600);
+	if (i ==0) {
+		playSound ("moo.wav");
+		System.out.println("moooooo");
+	}
+}
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
